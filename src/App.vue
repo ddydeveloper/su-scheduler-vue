e5c66a7a88b6107c9cf8ebc59db75d7d8e034a95
Daniil <ddydeveloper@gmail.com>
2018-12-04 19:35:28 +0300

    disabled tab

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <el-col :span="16" :offset="4">
       <el-menu :default-active="'1'" class="el-menu-demo" mode="horizontal" :router="true">
         <el-menu-item index="1" :route="{name:'users'}">Users</el-menu-item>
-        <el-menu-item index="2" :route="{name:'meetings'}">Meetings</el-menu-item>
+        <!-- <el-menu-item index="2" :route="{name:'meetings'}">Meetings</el-menu-item> -->
       </el-menu>
       <router-view/>
     </el-col>
